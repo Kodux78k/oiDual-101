50df8d5b7334702e0a92725cb24d05c438d4c455
Kodux78k <koduxlive@gmail.com>
2026-01-16 03:24:56 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Infodose conectado", {"ts":1768544363252,"id":"be317c0c-4552-40b6-b398-2860253befb9","meta":{"app":"generated.app"}});
+import "./modules/inline-0.js";
+import "./modules/inline-1.js";
+import "./modules/inline-2.js";
+import "./modules/inline-3.js";
+import "./modules/inline-4.js";
+import "./modules/inline-5.js";
+import "./modules/inline-6.js";
